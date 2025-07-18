48e3d2f9b6365b4be2bd575cef649ae9fa8d3e5d
Sandip Nayak <sandipnayak01@outlook.com>
2025-07-18 12:46:21 +0530

    bottom herosecton delete

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -73,10 +73,7 @@ const HeroSection = () => {
     
     </div>
 </section>
- <Link to="/demo"><button className=" lg:inline hidden md:block bg-white text-[#1233c1] text-sm hover:opacity-90 active:scale-95 transition-all w-40 md:h-11 h-12 mx-5 my-4 rounded-md font-bold">
-          Request a Demo
-        </button>
-    </Link>
+ 
 </div>
   )
 }
